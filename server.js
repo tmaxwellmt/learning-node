23e59bc9ecc32b2d52fa1c704c22d8a03cb2526e
tmaxwellmt <tmaxwellmt@gmail.com>
2017-03-21 21:32:28 -0600

    starting local host with node server

--- server.js
+++ server.js
@@ -1,1 +1,6 @@
-console.log("Hello");
+var http = require('http');
+
+http.createServer(function(req, res) {
+  response.end("hello world");
+  console.log("Your server is running on port 3000");
+}).listen(3000);
