48297f401b5bca29b1f1284490ae9cc366d9a90b
tmaxwellmt <tmaxwellmt@gmail.com>
2017-03-22 21:10:15 -0600

    introduction to express and working with app.get to get info from server. show on local host.

---
+++ README.md
@@ -0,0 +1,12 @@
+Node project must have a package.JSON file
+run the command with 'npm init' to make it.
+
+Install express for every node project through command line using 'npm install --save express'
+
+
+These are required for express to access server:
+var express = require("express");
+var app = express();
+
+App.GET example
+client makes a request, server responds
--- server.js
+++ server.js
@@ -1,6 +1,20 @@
-var http = require('http');
+var express = require("express");
+var app = express();
 
-http.createServer(function(req, res) {
-  response.end("hello world");
-  console.log("Your server is running on port 3000");
-}).listen(3000);
+// app.get is 
+
+app.get('/greeting', function (req, res) {
+  res.send("You found the greeting endpoint")
+});
+
+app.get('/happy', function (req, res) {
+  res.send("Your Happy")
+});
+
+app.get('/sad', function (req, res) {
+  res.send("Your Sad")
+});
+
+var server = app.listen(3000, function () {
+  console.log("App fired up on port 3000")
+});
